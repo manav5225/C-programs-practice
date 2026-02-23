f96ab80f6aa66dbb052cb6d3d01bf7cb9a01cf4e
Manav Thorve <109357648+manav5225@users.noreply.github.com>
2026-02-23 18:22:38 +0530

    Create vowelOrConsonant.c

---
+++ Conditional/vowelOrConsonant.c
@@ -0,0 +1,12 @@
+//==============================================VOWEL AND CONSONANT===============================================
+int main(){
+    char ch;
+    printf("Enter an Alphabet");
+    scanf("%s",&ch);
+    if(ch=='a'||'e'||'i'||'o'||'u'){
+        printf("Entered Alphabet is a Vowel ");
+    }
+    else{
+        printf("Enteres Alphabet is a consonant ");
+    }
+}
