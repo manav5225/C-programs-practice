f4082481367b02713a5f0175a26a6891a50ecc12
Manav Thorve <109357648+manav5225@users.noreply.github.com>
2026-02-23 18:21:48 +0530

    Create evenOrOddIfNegNo.c

---
+++ Conditional/evenOrOddIfNegNo.c
@@ -0,0 +1,17 @@
+======================================EVEN OR ODD--Number is negative nested if ===========================================
+int main(){
+    int num;
+    printf("Enter a Number\n");
+    scanf("%d",&num);
+
+    if(num<=0){
+        if(num%2==0){
+            printf("Even Number");
+        }else{
+            printf("Odd Number");
+        }
+    }
+    {
+        printf("Number is Negative\n");
+    }
+}
