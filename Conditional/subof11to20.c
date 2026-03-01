df507403f29ef8a5e36b1406983ba79062ff1ba2
Manav Thorve <109357648+manav5225@users.noreply.github.com>
2026-03-01 13:27:40 +0530

    Create subof11to20.c

---
+++ Conditional/subof11to20.c
@@ -0,0 +1,22 @@
+//=======================================================sub of n natural no from 11 to 20====================================================
+#include<stdio.h>
+int main(){
+    int i=11;
+    int j=12;
+    int sub=0;
+    do{
+       sub=i-j;
+       j++;
+    }while(j<=20);
+    printf("%d",sub);
+}
+int main(){
+    int a=12;
+    int sub=11;
+    do{
+        
+        sub=sub-a;
+        a++;
+    }while(a<=20);
+    printf("Sub of Number is : %d",sub);
+}
